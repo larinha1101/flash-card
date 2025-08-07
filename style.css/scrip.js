3ec29b03f526a8b257ceb503950267f0dc163e30
larinha1101 <lara.aparecida.oliveira@escola.pr.gov.br>
2025-08-07 20:36:45 -0300

    jean

---
+++ style.css/scrip.js
@@ -0,0 +1,20 @@
+const flashcards = [
+    {
+        pergunta: "Quem sou eu?",
+        categoria: "",
+        resposta: "Meu nome é Lara. Todas as coisas que aconteceram comigo formam quem sou eu."
+    },
+    {
+        pergunta: "Futuro - Como estarei em 10 anos?",
+        categoria: "",
+        resposta: "Estarei estável financeiramente, casada com meu namorado divo, e juntos poderemos realizar nossos sonhos."
+    },
+    {
+        pergunta: "Meu Presente - Como está sendo meu presente?",
+        categoria: "",
+        resposta: "Estou namorando há alguns meses e me sinto extremamente feliz. Trabalho na Makita e estou cursando Auxiliar Administrativo."
+    },
+    {
+        pergunta: "Minha Escola - Como está sendo minha experiência escolar?",
+        categoria: "",
+        resposta: "Estou no segundo ano do ensino médio 
